b223f50b6720d7242afbcac8d00299353fb578f3
Dalton Dick <daltondick2010@gmail.com>
2013-12-24 16:54:54 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boardgames::Application.config.secret_key_base = '7b99491084068210b1fc7f593e85147ee8ae03e6e84e87011a6f8a9e110d3ab9733f9bb81f3927fd00383bf85251fb408c7ffc278c8eef18b5c08d753e20c4b7'

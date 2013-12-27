64db2da8ded76cb1ac712823ae8d8cf30114c6c1
Dalton Dick <daltondick2010@gmail.com>
2013-12-27 00:47:15 -0600

    Add user stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Boardgames::Application.routes.draw do
+  get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

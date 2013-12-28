e2e4ec009ed4bf1ba2796ef9c1c7e78081a04eec
Dalton Dick <daltondick2010@gmail.com>
2013-12-28 02:25:10 -0600

    Added boardgame index test

--- spec/features/board_game_pages_spec.rb
+++ spec/features/board_game_pages_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe "Board Game pages" do
+	
+	subject { page }
+
+	describe "Index page" do
+		let!(:bg1) { FactoryGirl.create(:board_game) }
+		let!(:bg2) { FactoryGirl.create(:board_game) }
+
+		before { visit board_games_path }
+		
+		it { should have_content(bg1.name) }
+		it { should have_content(bg2.name) }
+	end
 end

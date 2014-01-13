7767d2ce48f02184c477840789efda6c375434b3
Dalton Dick <daltondick2010@gmail.com>
2014-01-13 15:06:20 -0600

    Reverse sort game plays on board games

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   	has_many :players
 
 	before_save { self.email = email.downcase }
-	# before_create :create_remember_token
 
  	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -20,10 +19,4 @@ class User < ActiveRecord::Base
 	def User.encrypt(token)
 		Digest::SHA1.hexdigest(token.to_s)
 	end
-
-	# private
-
-	# 	def create_remember_token
-	# 		self.remember_token = User.encrypt(User.new_remember_token)
-	# 	end
 end

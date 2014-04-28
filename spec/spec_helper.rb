d3304fb592e126b4e557d8934a4b283465b49e4c
Dalton Dick <daltondick2010@gmail.com>
2014-04-27 20:38:56 -0500

    Start SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ if ENV['CIRCLE_ARTIFACTS']
   require 'simplecov'
   dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
+  SimpleCov.start 'rails'
 end
 
 
